496c03f8a44e51b46b5e4c5cb4e98d5ae967b078
maxpanic <kurogane@tomail.org>
2013-11-13 20:32:36 -0700

    lab6 update
    updation

--- LAB6/MyString.cpp
+++ LAB6/MyString.cpp
@@ -49,85 +49,89 @@ char MyString::at(int index) const
   return storageM[index];
 }
 
+// Compare storageM to a given string pointer.
 bool MyString::equals(const char *b) const
 {
 	int i=0;
-	int newL;
-	while(*b != '\0')
+	int newL=0;
+	while(*b != '\0') // Find the length of the pointer.
 	{
 		newL++;
 		b++;
 	}
-	b -=newL;
+	b -=newL; // Set the pointer head back to zero.
 
-	if (lengthM != newL)
+	if (lengthM != newL) // Quick check to compare the lengths of sizeM and the string pointer.
 		return false;
 
-	do
+	do // Compare each string position char by char.
 	{
 		if(at(i) !=*b)
 			return false;
 		b++;
 		i++;
-	} while(storageM[i] != '\0');
+	// Since we have already asserted that the lengths must be equal we only to be limited by
+	// one string length. This assumes \0 is only used once, at the end of the string. 
+	} while(storageM[i] != '\0'); 
 	return true;
 }
 
+// Compare storageM to another MyString.
 bool MyString::equals(const MyString& other) const
 {
 	int i=0;
 
-	if (length() != other.length())
+	if (length() != other.length()) // Compare the lengths for easy elimination.
 		return false;
 
-	do
+	do // Compare what is in each string char by char.
 	{
 		if(at(i) != other.at(i))
 			return false;
 		i++;
+	// Since we have already asserted that the lengths must be equal we only to be limited by
+	// one string length. This assumes \0 is only used once, at the end of the string. 
 	} while(storageM[i] != '\0');
 	return true;
 }
 
+// Set a char in any position except for the last to a given replacement.
 void MyString::set_char_at(int index, char replacement)
 {
-	assert(0 <= index && index < length());
+	assert(0 <= index && index <= length()); // Assert that the index is within the strings limits. Ammended from the .h file to make it more usefull.
 	storageM[index] = replacement;
 }
 
+// Append one string to the end of storageM, up until the end of the MAX_LENGTH or the end of b.
 void MyString::append(const char *b)
 {
-
 	while(lengthM < MAX_LENGTH && *b != '\0')
 	{
-
-		storageM[lengthM] = *b;
+		set_char_at(lengthM, *b); // Append the beginning of b to the end of storageM.
 		lengthM++;
 		b++;
-
 	};
-	storageM[lengthM] ='\0';
-
+	storageM[lengthM] ='\0'; // Tack on the limiter.
 }
 
+// Append the contents of other to storageM.
 void MyString::append(const MyString& other)
 {
 	int i=0;
 	while(lengthM < MAX_LENGTH && other.storageM[i] != '\0')
 		{
-
-			storageM[lengthM] = other.storageM[i];
+			set_char_at(lengthM, other.at(i)); // Append the beginning of other to the end of storageM.
 			lengthM++;
 			i++;
-
 		};
 		storageM[lengthM] ='\0';
 }
 
+// Truncate storageM.
 void MyString::truncate(int shorter_length)
 {
-	assert(0 <= shorter_length && shorter_length <= length());
+	assert(0 <= shorter_length && shorter_length <= length()); // Assert that shorter_length is within working bounds.
 
-	storageM[shorter_length] = '\0';
+	storageM[shorter_length] = '\0'; // Simply add the limiter to the desired position, destructive but effective.
 	lengthM = shorter_length;
 }
--- LAB6/MyStringTwo.cpp
+++ LAB6/MyStringTwo.cpp
@@ -58,45 +58,45 @@ void MyStringTwo::set_char(int pos, char c)
 
 void MyStringTwo::append(const MyStringTwo& tail)
 {
-	int i=0;
+	int i=0; // Set a counter to 0.
 	if (tail.length() != 0)
 	{
-		int j = lengthM;
-		MyStringTwo temp(storageM);
-		this->~MyStringTwo();
-		storageM = new char[(lengthM += tail.lengthM) + 1];
-		strcpy(storageM, temp.c_str());
+		int j = lengthM; // Set a counter to the length of the first string.
+		MyStringTwo temp(storageM); // Make a temp string and set it to hold the first string.
+		this->~MyStringTwo(); // Delete the fisrt string.
+		storageM = new char[(lengthM += tail.lengthM) + 1]; // Reallocate memory to storageM, whilst increasing lengthM to the proper size.
+		strcpy(storageM, temp.c_str()); // Copy what was in temp to the reallocated storageM.
 		while (i < tail.length())
 		{
-			this->set_char((j),tail.get_char(i));
+			this->set_char((j),tail.get_char(i)); // Starting at where the original string left off, add tail.
 			i++;
 			j++;
 		}
-		tail.~MyStringTwo();
+		temp.~MyStringTwo(); // Deallocate temp.
 	}
 
 }
 
 void MyStringTwo::truncate(int new_length)
 {
-	assert(0 <= new_length && new_length <= length());
+	assert(0 <= new_length && new_length <= length()); // Make sure new_length is non-negative.
 	int i=0;
-	MyStringTwo temp(storageM);
-	if(new_length != 0)
+	MyStringTwo temp(storageM); // Make a temp string and set it to hold the first string.
+	if(new_length != 0) // For non-zero truncations.
 	{
 
-		this->~MyStringTwo();
-		storageM = new char[new_length+1];
-		lengthM = new_length;
+		this->~MyStringTwo(); // Deallocate this.
+		storageM = new char[new_length+1]; // Reallocate to new size.
+		lengthM = new_length; // Set length to new size.
 		while (i < new_length)
 		{
-			storageM[i] = temp.get_char(i);
+			storageM[i] = temp.get_char(i); // Copy temp into the new storageM up until the new size.
 			i++;
 		}
 	}
-	else
+	else // For truncation at zero, have to bypass set_char because of assertion.
 	{
 		storageM[i]='\0';
 	}
-	temp.~MyStringTwo();
+	temp.~MyStringTwo(); // Deallocate temp.
 }
